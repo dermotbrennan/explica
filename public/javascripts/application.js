cff4fd0e7692de7d606c770f004c544e6b426f42
Dermot Brennan <dermot.brennan@gmail.com>
2010-06-01 01:58:49 +0100

    Improved layout of wikipedia article. Made site full width

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,10 +6,11 @@
     //$("#main_content").splitter();
     $("#main_content>div").equalHeights(jQuery(document).height() - 41);
 
-    resize_original = function(event, ui){
-        inner_els = $("#main_content #sidebar_right, #main_content #original_text");
-        $('#sidebar_right').width((jQuery('#primary_wrapper').width()) - $('#original_text').width() - 3);
+    var inner_els = $("#main_content #sidebar_right, #main_content #original_text");
+    resize_original = function(event, ui) {
+        $('#sidebar_right').width((jQuery('#primary_wrapper').width()) - $('#original_text').width() - 28);
         //inner_els.css("height", "auto");
+
         inner_els.equalHeights(jQuery(document).height() - 60);
       }
 
@@ -18,8 +19,7 @@
       start: resize_original
     });
 
-    $('#sidebar_right').width((jQuery('#primary_wrapper').width()) - $('#original_text').width() - 3);
-
+    $('#sidebar_right').width((jQuery('#primary_wrapper').width()) - $('#original_text').width() - 28);
 
 
     words = $("#original_text span.word");
@@ -30,6 +30,7 @@
         $('#sidebar_right_inner').prepend("<h2>"+ word + " - loading...</h2>");
 
         $('#sidebar_right_inner').load('/definitions/'+ word, function() {
+          inner_els.css("overflow", "auto");
         });
       })
     }
